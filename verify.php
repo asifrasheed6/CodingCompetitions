31aa1f230c10114fc9b67b5ccbef6bf4be9f5bb7
Asif Rasheed <b00073877@aus.edu>
2020-03-07 18:43:19 +0400

    Added Email Verification for Sign Up

--- verify.php
+++ verify.php
@@ -3,7 +3,10 @@
     $hash = $_GET['ac'];
     $query = mysqli_query($database, "SELECT * FROM `Verify` WHERE `hash` = '$hash'");
     
-    if(mysqli_num_rows($query)==0) header('location: submit.php?email=&status=false');
+    if(mysqli_num_rows($query)==0){
+        header('location: submit.php?email=&status=false');
+        exit;
+    }
     
     $row = mysqli_fetch_array($query);
     $id = $row['id'];
